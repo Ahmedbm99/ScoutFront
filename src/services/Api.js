1d7801ce66cc9e024655590f4c6cf98d54f179cd
Ahmedbm99 <101886914+Ahmedbm99@users.noreply.github.com>
2025-07-09 02:01:50 +0100

    first commit

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const createApi = () => {
   return axios.create({
-    baseURL: `http://localhost:5000`,
+    baseURL: `https://scoutback.onrender.com`,
     withCredentials: true,
   });
 };
